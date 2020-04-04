be7521597843f000f350ec8e1d0ab393041b6b4e
tores <tore.sveaass@gmail.com>
2020-04-05 00:00:35 +0200

    la til oppmote-skjema component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OppmoteSkjemaComponent } from './oppmote-skjema/oppmote-skjema.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OppmoteSkjemaComponent
   ],
   imports: [
     BrowserModule,
